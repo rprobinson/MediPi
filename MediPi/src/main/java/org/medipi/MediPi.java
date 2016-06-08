d8b2797cefc13c54119a73a9b92ed5b20b4d6517
Krishna Kuntala <kuntala.krishna@mastek.com>
2016-06-08 16:18:50 +0100

    Changed version details in MediPi.java file

--- MediPi/src/main/java/org/medipi/MediPi.java
+++ MediPi/src/main/java/org/medipi/MediPi.java
@@ -90,8 +90,8 @@ public class MediPi extends Application {
 
     // MediPi version Number
     private static final String MEDIPINAME = "MediPi Telehealth System";
-    private static final String VERSION = "MediPi_v1.0.6";
-    private static final String VERSIONNAME = "PILOT-20160601-1";
+    private static final String VERSION = "MediPi_v1.0.7";
+    private static final String VERSIONNAME = "PILOT-20160608-1";
 
     // Set the MediPi Log directory
     private static final String LOG = "medipi.log";
